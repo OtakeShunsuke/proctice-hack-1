e1e4fe20c9bbad64224fe95ae2022c4e966eb3d4
OtakeShunsuke <56343986+OtakeShunsuke@users.noreply.github.com>
2023-07-13 19:53:27 +0900

    fix: 文字コードの修正

--- build-docker-container.bash
+++ build-docker-container.bash
@@ -1,6 +1,5 @@
-cd backend_Project
-docker-compose run backend django-admin startproject backend .
-cd ../frontend_Project
+cd backend_Project/ && docker-compose run backend django-admin startproject backend .
+cd ../frontend_Project/
 docker-compose run --rm frontend sh -c "npm install -g create-react-app && create-react-app frontend"
 cd ..
 docker-compose down
