7088bf656ece7b2aca8a8d2893bd2a360e92e847
OtakeShunsuke <s.ootake.f3@icloud.com>
2023-07-13 12:38:39 +0900

    fix: ビルドに伴うコマンドの修正

--- build-docker-container.bash
+++ build-docker-container.bash
@@ -3,4 +3,5 @@ docker-compose run backend django-admin startproject backend .
 cd ../frontend_Project
 docker-compose run --rm frontend sh -c "npm install -g create-react-app && create-react-app frontend"
 cd ..
+docker-compose down
 docker-compose build --no-cache
