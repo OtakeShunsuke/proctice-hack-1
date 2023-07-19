36ab5952df70002b54dca958081ab7284c80675f
OtakeShunsuke <s.ootake.f3@icloud.com>
2023-07-19 11:09:11 +0900

    fix: 環境構築の修正

--- build-docker-container.bash
+++ build-docker-container.bash
@@ -1,5 +1,8 @@
 cd backend_Project/ && docker-compose run backend django-admin startproject backend .
-cd ../frontend_Project/
+cd ..
+docker-compose build --no-cache
+docker-compose up -d
+cd frontend_Project/
 docker-compose run --rm frontend sh -c "npm install -g create-react-app && create-react-app frontend"
 cd ..
 docker-compose down
